9cb145da4121eb4f6c4255f75f8266eeb069f28f
Chris J.M <chris@euchre.us>
2015-12-01 18:06:13 -0600

    Initial Commit

---
+++ Swift Euchre/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Euchre
+//
+//  Created by Chris Matlak on 12/1/15.
+//  Copyright © 2015 Euchre US!. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
